486aadb7ae1e22f86725ce6e60eee627d634d6a8
Aron Smit <aron.smit@hva.nl>
2019-07-12 10:23:26 +0200

    Add admin aron

---
+++ cms/.users.js
@@ -0,0 +1,11 @@
+({
+	users: [
+		{
+			username: 'aron',
+			tags: [],
+			salt: 'ffe00ca0f08ac5795608e4dece2e3805',
+			hash: 'e69d19d1f9d845c9d004c3253d85b2861dcd24f5c51495516dd0088be2107cd1',
+			user_created_timestamp: 1562919783840
+		}
+	]
+})
